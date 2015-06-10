70d0f0fbff13d951d2206e4787f810ae81846533
christalin <christy0309@gmail.com>
2015-06-10 16:23:49 -0500

    Added database seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "tester", email: "tester@gmail.com", password: "christu09", password_confirmation: "christu09")
+User.create(username: "tester1", email: "tester1@gmail.com", password: "christu09", password_confirmation: "christu09")
+User.create(username: "tester2", email: "tester2@gmail.com", password: "christu09", password_confirmation: "christu09")
+User.create(username: "tester3", email: "tester3@gmail.com", password: "christu09", password_confirmation: "christu09")
+User.create(username: "tester4", email: "tester4@gmail.com", password: "christu09", password_confirmation: "christu09")
+p "Test users created"
