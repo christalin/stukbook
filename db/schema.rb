c01ec80ab76d9fc78cf3a0f71714d1d3797a32aa
christalin <christy0309@gmail.com>
2015-06-11 15:17:24 -0500

    Added friendships

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610022008) do
+ActiveRecord::Schema.define(version: 20150611194304) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.string   "state",       default: "pending"
+    t.datetime "friended_at"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "username",               default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -26,7 +36,6 @@ ActiveRecord::Schema.define(version: 20150610022008) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "username"
     t.text     "bio"
     t.integer  "age"
     t.string   "gender"
@@ -34,5 +43,6 @@ ActiveRecord::Schema.define(version: 20150610022008) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
